5b12671b381d61f7a5e7c464420167c24d3c324d
Julio Lira <jul10l1r4@gmail.com>
2018-02-16 14:48:30 -0300

    Efeito scroll pela página

--- Ecmascript/scroll.js
+++ Ecmascript/scroll.js
@@ -1,10 +1,15 @@
 let a = document.querySelector( `footer` )
 let b = document.querySelector( `section` )
 // Efeito scroll, rolagem da página, pega a barra na esquerda e muda ela para ficar fixa
-const Scroll = () => // Arrow function `ES6`
-		b.setAttribute( `style`, `opacity:1` )
-		a.setAttribute( `style`, `opacity:1` )
-
+const Scroll = () => {// Arrow function `ES6`
+    if(window.pageYOffset + ((window.innerHeight * 3) / 4) > b.offsetTop) {
+			b.setAttribute( `style`, `opacity:1;transition-duration:2s` )
+			a.setAttribute( `style`, `opacity:1;transition-duration:4s;` )
+		}else{
+			b.setAttribute( `style`, `opacity:0` )
+			a.setAttribute( `style`, `opacity:0` )
+		}
+}
 // Ativa a nossa função
 window.addEventListener( `scroll`, Scroll );
 
