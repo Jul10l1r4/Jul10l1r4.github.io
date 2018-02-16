e53455361a2e9b6cf3f5e757e4853a4f804188f6
Julio Lira <jul10l1r4@gmail.com>
2018-02-16 19:47:57 -0300

    Scroll da página, para o efeito

--- Ecmascript/scroll.js
+++ Ecmascript/scroll.js
@@ -1,7 +1,7 @@
 let a = document.querySelector( `footer` )
 let b = document.querySelector( `section` )
 // Efeito scroll, rolagem da página, pega a barra na esquerda e muda ela para ficar fixa
-const Scroll = () => {// Arrow function `ES6`
+const Scroll = () => {// Arrow function `ES6`, calcula a posição para que mostre o efeito
     if(window.pageYOffset + ((window.innerHeight * 3) / 4) > b.offsetTop) {
 			b.setAttribute( `style`, `opacity:1;transition-duration:2s` )
 			a.setAttribute( `style`, `opacity:1;transition-duration:4s;` )
@@ -12,4 +12,8 @@ const Scroll = () => {// Arrow function `ES6`
 }
 // Ativa a nossa função
 window.addEventListener( `scroll`, Scroll );
-
+// verfifica o browser, pq se for no cu do cacete do chrome vai ser bugadão, entao é melhor ne ter
+let c = document.querySelector( `#down` )
+if (navigator.userAgent.indexOf("Chrome") > -1){
+	document.querySelector( `#down` ).setAttribute( `style`, `display:none` )
+}
