832aa708f0c89fd4c4644de10092396b840dbc1c
Julio Lira <jul10l1r4@gmail.com>
2018-03-15 01:00:01 -0300

    Criação da homepage ^^

---
+++ Ecmascript/analiz.js
@@ -0,0 +1,49 @@
+const enviar = (nome, email, mensagem, lugar) => {
+
+	let nom = document.querySelector(nome).value
+	let emai = document.querySelector(email).value
+	let mensage = document.querySelector(mensagem).value
+
+	let compos = encodeURIComponent(
+	`
+		<strong>Nova mensagem enviada</strong>
+			Nome: <strong>${nom}</strong> \r
+			Email:  <strong>${emai}</strong> \r
+			Mensagem: <strong>${mensage}</strong>
+		
+	`)
+	fetch(
+	`https://api.telegram.org/bot540137695:AAFO9tHLngrkve3mHN7QHdzmWyJs_O8-x3k/sendMessage?chat_id=469669662&text=${compos}&parse_mode=html`
+	).then((resposta) => {
+	return resposta.text() // Transformando a resposta em texto
+		.then((text) => {
+			let t = JSON.parse(text);
+			let uhul = t.ok === false
+				? `Oops, infelizmente houve um erro`
+				: 'Mensagem enviada ^^';
+			let pure = document.querySelector(lugar).innerHTML
+			document.querySelector(lugar).setAttribute("style", "animation-name:sair;animation-duration:0.6s")
+			setTimeout(()=>{
+				document.querySelector(lugar).innerHTML = `
+					<div class='tcenter cbranco' id='mensagem'>
+						<span class='msg'>${uhul}</span>
+						<h2>formas de contato <img src="https://emojipedia-us.s3.amazonaws.com/thumbs/160/microsoft/74/pencil_270f.png"></h2>
+							<p style="text-align:center">
+								Acesse meu github: <a href="https://github.com/Jul10l1r4/" target="_blank">Jul10l1r4</a><br/>
+								Conheça meu git principal ❤: <a href="https://notabug.org/Jul10l1r4/" target="_blank">Jul10l1r4</a>.<br/>
+							</p>
+							<h2></h2>
+							<address>
+								Fale comigo no telegram <img src="https://emojipedia-us.s3.amazonaws.com/thumbs/160/mozilla/36/rocket_1f680.png"> <a href="https://web.telegram.org/#/im?p=@Jul10l1r4" target="_blank">@jul10l1r4</a>.<br/>
+								Mande-me um email <img src="https://emojipedia-us.s3.amazonaws.com/thumbs/160/mozilla/36/envelope_2709.png"> <a href="mailto:jul10l1r4@ufrn.edu.br" target="_blank">jul10l1r4@ufrn.edu.br</a>.
+							</address>
+					</div>`
+				},600)
+		})
+	})
+	
+
+}
+//const retornar = (seletor, buf) => {
+//	document.querySelector(seletor).innerHTML = buf
+//}
--- Ecmascript/scroll.js
+++ Ecmascript/scroll.js
@@ -1,24 +1,36 @@
 let b = document.querySelector( `section` )
-let a = document.querySelector( `footer` )
 // Efeito scroll, rolagem da página, pega a barra na esquerda e muda ela para ficar fixa
 const Scroll = () => {// Arrow function `ES6`, calcula a posição para que mostre o efeito
     if(window.pageYOffset + ((window.innerHeight * 3) / 4) > b.offsetTop) {
 			b.setAttribute( `style`, `opacity:1;transition-duration:2s` )
-			a.setAttribute( `style`, `opacity:1;transition-duration:4s;` )
 		}else{
-			b.setAttribute( `style`, `opacity:0` )
-			a.setAttribute( `style`, `opacity:0` )
+			b.setAttribute( `style`, `opacity:0;transition-duration:2s` )
 		}
 }
 // Ativa a nossa função
 setTimeout(
 	window.addEventListener( `scroll`, Scroll ),500
 )
+// Ativa a nossa função
+setTimeout(
+	window.addEventListener( `scroll`, Scroll ),500
+)
 const sobre = () => {
   window.scroll({ // Função nativa dos browsers
-	  top: document.querySelector( `#conteudo` ).getBoundingClientRect().top, // Captura a quantidade precisa de px do elemento em cada tela diferente fiii
+	  top: document.querySelector( `#conteudo` ).offsetTop, // Captura a quantidade precisa de px do elemento em cada tela diferente fiii
 	  left: 0,// Obrigatório, nao queremos movimentos horizontais
 	  behavior: 'smooth' // Unico efeito, dá a instrução ao browser que ele movaaaa uhuuull caraaai
    });
 }
 document.querySelector( `#to` ).addEventListener( `click`, sobre );
+
+// Função de rolagem suave
+const go = (elemento) => {// Arrow function levando um parâmetro que será um seletor como o de css
+  let vivo = document.querySelector(elemento)// Pegará esse seletor e retornará um elemento vivo
+  window.scroll({ // Função nativa dos browsers
+	  top: vivo.offsetTop, // Captura a quantidade precisa de px do elemento em cada tela diferente fiii
+	  left: 0,// Obrigatório, nao queremos movimentos horizontais
+	  behavior: 'smooth' // Unico efeito, dá a instrução ao browser que ele movaaaa uhuuull caraaai
+  });// terminouuu fiii, só isso, c n vai precisar exporrtar mais 5000mil instruçoes de dados da porra do jquery
+} 
+
