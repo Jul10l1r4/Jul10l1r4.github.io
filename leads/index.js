b684873d33ec8845c02bf8abad61b66bc17926b6
julio <julio@machine.julio>
2018-03-31 09:46:48 -0300

    update

--- leads/index.js
+++ leads/index.js
@@ -4,7 +4,7 @@ const send = () => {
 	let message = encodeURIComponent(document.querySelector(` [name='message']`).value)
 	let dest = encodeURIComponent(document.querySelector(` [name='dest']`).value)
 	fetch(
-		`http://api-email.000webhostapp.com/?name=${name}?email=${email}?message=${email}?message=${message}?dest=${dest}`
+		`https://api-email.000webhostapp.com/?name=${name}?email=${email}?message=${email}?message=${message}?dest=${dest}`
 			).then(function(resposta) {
 		return resposta
 			.text() // Transformando a resposta em texto
