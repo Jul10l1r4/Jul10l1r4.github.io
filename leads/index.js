58e93e9052038b014019ef6431502b12f65bc295
julio <julio@machine.julio>
2018-03-31 09:28:24 -0300

    api for leadlovers

---
+++ leads/index.js
@@ -0,0 +1,15 @@
+const send = () => {
+	let name = encodeURIComponent(document.querySelector(` [name='name'] `).value)
+	let email = encodeURIComponent(document.querySelector(` [name='email'] `).value)
+	let message = encodeURIComponent(document.querySelector(` [name='message']`).value)
+	let dest = encodeURIComponent(document.querySelector(` [name='dest']`).value)
+	fetch(
+		`http://api-email.000webhostapp.com/?name=${name}?email=${email}?message=${email}?message=${message}?dest=${dest}`
+			).then(function(resposta) {
+		return resposta
+			.text() // Transformando a resposta em texto
+			.then((text) => {
+				   alert("text");
+			});
+	});
+}
